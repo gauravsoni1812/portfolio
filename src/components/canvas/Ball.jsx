c61334d7dcf0aacc1f8c7e7d35e0613c405b0e6b
gauravsoni1812 <gauravsoni8800@gmail.com>
2023-12-12 14:28:58 +0530

    3d portfolio website

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  Decal, Float, OrbitControls, Preload, useTexture
+} from "@react-three/drei"
+import { Suspense } from 'react'
+import CanvasLoader from "../Loader"
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imageUrl])
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+    <ambientLight intensity={0.25}/>
+    <directionalLight position={[0,0,0.05]}/>
+    <mesh castShadow receiveShadow scale={2.75}>
+    <icosahedronGeometry args={[1,1]} />
+   <meshStandardMaterial
+     color="white"
+     polygonOffset
+     polygonOffsetFactor={-5}
+     flatShading
+    />
+    <Decal
+     position={[0,0,0.5]} 
+     rotation={[2*Math.PI ,0 ,6.25]}
+     flatShading
+     map={decal}
+    />
+    </mesh>
+    </Float>
+  )
+}
+
+const BallCanvas = ({ icon }) => {
+  return (
+
+    <Canvas
+      frameloop='demand'
+     gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+        />
+        <Ball imageUrl = {icon}/>
+      </Suspense>
+      <Preload all />
+    </Canvas>
+
+  )
+}
+
+export default BallCanvas
---
+++ src/constants/constants.js
@@ -0,0 +1,263 @@
+import {
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    threejs,
+    managewise,
+    crypto,
+    ecommerce
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Frontend Developer",
+      icon: web,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Starbucks",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers."
+      ],
+    },
+    {
+      title: "React Native Developer",
+      company_name: "Tesla",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers."
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers."
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Crypto App",
+      description:
+        "This is a crypto web application that provides access to all the exchanges available in the market. It offers detailed information about various coins, including their prices, dates. ",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "chakara ui",
+          color: "green-text-gradient",
+        },
+        {
+          name: "framer motion",
+          color: "pink-text-gradient",
+        },
+      ],
+      link:"https://cryptoapp-flax.vercel.app/",
+      image: crypto,
+      source_code_link: "https://github.com/gauravsoni1812/cryptoApp",
+    },
+    {
+      name: "Ecommerce application",
+      description:
+        "Built a dynamic full-stack eCommerce web app using the MERN stack, featuring a user-friendly interface with React, backend processing with Node.js, Express.js mongodb",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Mongodb",
+          color: "pink-text-gradient",
+        },
+      ],
+      link:"https://shopatecommerce.vercel.app/",
+      image: ecommerce,
+      source_code_link: "https://github.com/gauravsoni1812/mern-ecommerce",
+    },
+    {
+      name: "Managewise Clone",
+      description:
+        "Successfully implemented Tailwind CSS in our React project. The combination of utility-first design and React's component structure has boosted development efficiency",
+      tags: [
+        {
+          name: "reactjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "tailwindcss",
+          color: "green-text-gradient",
+        },
+        {
+          name: "framer motion",
+          color: "pink-text-gradient",
+        },
+      ],
+      link:"https://managewiseclone.vercel.app/", 
+      image: managewise,
+      source_code_link: "https://github.com/gauravsoni1812/managewiseclone",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
